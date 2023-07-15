3f6312297759c2c7543759e61ba3df9b30294fb8
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-07-15 22:47:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SujanCh564/DemoBot.git /DemoBot
+  git clone https://github.com/Gowdru123/DemoBotx.git /DemoBotx
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DemoBot
+  git clone $UPSTREAM_REPO /DemoBotx
 fi
-cd /DemoBot
+cd /DemoBotx
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
