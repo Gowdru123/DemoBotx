2231ecc4b41a3c6aa2aa6f781aabd875b7202ad2
SujanCh564 <132272276+SujanCh564@users.noreply.github.com>
2023-07-15 21:45:12 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/SujanCh564/DemoBot.git /DemoBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /DemoBot
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
